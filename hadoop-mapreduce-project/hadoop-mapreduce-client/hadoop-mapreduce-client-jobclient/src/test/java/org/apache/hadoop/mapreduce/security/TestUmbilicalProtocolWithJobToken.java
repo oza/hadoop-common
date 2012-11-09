e7c61e63be87998fbdcdc660240182e2e9a9f3f9
OZAWA Tsuyoshi <ozawa.tsuyoshi@gmail.com>
2012-11-09 18:02:49 +0900

    commit to test to change extended umbilical protocol
    Signed-off-by: OZAWA Tsuyoshi <ozawa.tsuyoshi@gmail.com>

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient/src/test/java/org/apache/hadoop/mapreduce/security/TestUmbilicalProtocolWithJobToken.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient/src/test/java/org/apache/hadoop/mapreduce/security/TestUmbilicalProtocolWithJobToken.java
@@ -116,6 +116,7 @@ public class TestUmbilicalProtocolWithJobToken {
               TaskUmbilicalProtocol.class, TaskUmbilicalProtocol.versionID,
               addr, conf);
           proxy.ping(null);
+          proxy.startNewAggregation(null, null);
         } finally {
           server.stop();
           if (proxy != null) {
