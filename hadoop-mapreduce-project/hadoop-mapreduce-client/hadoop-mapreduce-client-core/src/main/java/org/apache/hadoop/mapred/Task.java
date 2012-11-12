62ec2e34ac27cf92de8dc5826092b675ae1642cc
OZAWA Tsuyoshi <ozawa.tsuyoshi@gmail.com>
2012-11-12 18:25:29 +0900

    added isAggregatingFlag to TaskAttemptID
    Signed-off-by: OZAWA Tsuyoshi <ozawa.tsuyoshi@gmail.com>

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapred/Task.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapred/Task.java
@@ -721,7 +721,7 @@ abstract public class Task implements Writable, Configurable {
             taskFound = umbilical.ping(taskId);
             // XXX node level aggregation.
             // This is just for testing.
-            umbilical.startNewAggregation(null, jobFile);
+            umbilical.startNewAggregation(taskId, null, jobFile);
           }
 
           // if Task Tracker is not aware of our task ID (probably because it died and 
--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapred/TaskUmbilicalProtocol.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapred/TaskUmbilicalProtocol.java
@@ -146,7 +146,7 @@ public interface TaskUmbilicalProtocol extends VersionedProtocol {
   void fatalError(TaskAttemptID taskId, String message) throws IOException;
   
   /** Start aggregation per node or rack.*/
-  void startNewAggregation(List<TaskAttemptID> taskIds, String message) throws IOException;
+  boolean startNewAggregation(TaskAttemptID aggregator, List<TaskAttemptID> taskIds, String message) throws IOException;
   
   /** Called by a reduce task to get the map output locations for finished maps.
    * Returns an update centered around the map-task-completion-events. 
