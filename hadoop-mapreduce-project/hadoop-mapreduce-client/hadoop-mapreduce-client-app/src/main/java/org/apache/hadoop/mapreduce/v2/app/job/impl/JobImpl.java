6677d7035f53254f42a843147adfec52e5837268
OZAWA Tsuyoshi <ozawa.tsuyoshi@gmail.com>
2012-12-01 01:14:42 +0900

    Fixed NullPointerException around aggregation code
    Signed-off-by: OZAWA Tsuyoshi <ozawa.tsuyoshi@gmail.com>

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-app/src/main/java/org/apache/hadoop/mapreduce/v2/app/job/impl/JobImpl.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-app/src/main/java/org/apache/hadoop/mapreduce/v2/app/job/impl/JobImpl.java
@@ -434,7 +434,8 @@ public class JobImpl implements org.apache.hadoop.mapreduce.v2.app.job.Job,
     this.appSubmitTime = appSubmitTime;
     this.oldJobId = TypeConverter.fromYarn(jobId);
     this.newApiCommitter = newApiCommitter;
-    this.isAggregationEnabled = false;
+    // TODO: Fix to pass by JobConf MR-4502
+    this.isAggregationEnabled = true;
 
     this.taskAttemptListener = taskAttemptListener;
     this.eventHandler = eventHandler;
@@ -1367,6 +1368,7 @@ public class JobImpl implements org.apache.hadoop.mapreduce.v2.app.job.Job,
               // MAPREDUCE-4902
               // , and start to fetch dummy file.
               for (TaskAttemptCompletionEvent ev:events) {
+                ev.setStatus(TaskAttemptCompletionEventStatus.AGGREGATED);
                 job.taskAttemptCompletionEvents.add(ev);
               }
             } catch (MalformedURLException e) {
