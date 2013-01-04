365b08456fd57393dda1703bf058319f3d743819
OZAWA <ozawa.tsuyoshi@lab.ntt.co.jp>
2013-01-04 16:54:55 +0900

    Changed Mappers and Reducers to support Node-level aggregation
    Signed-off-by: OZAWA <ozawa.tsuyoshi@lab.ntt.co.jp>

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/task/reduce/Fetcher.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/task/reduce/Fetcher.java
@@ -282,6 +282,10 @@ class Fetcher<K,V> extends Thread {
         scheduler.putBackKnownMapOutput(host, left);
       }
       
+      for(TaskAttemptID skipId: host.getAndClearSkippingMaps()) {
+        remaining.remove(skipId);
+      }
+      
       return;
     }
     
